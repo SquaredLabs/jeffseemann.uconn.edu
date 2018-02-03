abaae354b27194196835784208bd01ba3656b6fa
qvissak <quinn.vissak@uconn.edu>
2018-02-03 16:58:45 -0500

    JEFF-3 Sitewide Footer
    Requires updates when dynamic content is available.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
-import styles from './styles.scss'
+import './styles.css'
 
 class Footer extends Component {
     shouldComponentUpdate = () => false
+
+    // Replace with dynamic content from admin-on-rest
     render = () =>
-      <footer className={styles.footer}>
-        <div>Test</div>
+      <footer className="footer">
+        <div className="footer-item">jeff.seemann@uconn.edu</div>
+        <div className="footer-item">+1 860 555 5555</div>
+        <div className="footer-item">123 Drury Lane, Storrs, CT 06269</div>
       </footer>
 }
 
