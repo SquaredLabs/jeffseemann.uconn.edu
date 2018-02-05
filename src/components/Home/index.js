c99fd7684d663a17d6040a61e4e41407fb33dacc
qvissak <quinn.vissak@uconn.edu>
2018-02-05 00:52:25 -0500

    JEFF-5 Create Landing Page
    Hard code landing page to allow for routing and testing.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 
-import './styles.scss'
+import './styles.css'
 
 class Home extends Component {
     render = () =>
-      <div>
-
+      <div className="home-container">
+        <div className="text-container">
+          <h1 className="name-header">Jeff Seemann</h1>
+          <div>Jeff Seemann has been an active and respected member of
+          the nationwide research community for nearly four decades.
+          While his research expertise lies in the field of <a className="text-keyword">plant biology</a>,
+          he is a respected member and seasoned administrator, having held
+          positions at <a className="text-keyword">University of Nevado Reno</a>,
+          <a className="text-keyword"> University of Rhode Island</a>,
+          <a className="text-keyword"> Texas A&M</a>, and the <a className="text-keyword"> University of Connecticut</a>.</div>
+          <br/>
+          <div>Jeff is now running a lab at the University of Connecticut,
+          conducting research and looking to <a className="text-keyword"> hire </a>
+          and <a className="text-keyword"> collaborate</a>.</div>
+        </div>
+        <div className="side-container">
+          <div className="side-item">Lab</div>
+          <div className="side-item">Publications</div>
+          <div className="side-item">Reading</div>
+          <div className="side-item">Collaborative</div>
+          <div className="side-item">Work</div>
+          <div className="side-item">Content</div>
+        </div>
       </div>
 }
 
