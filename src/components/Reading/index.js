75a6fe4a6fb4807a1b7d1d9a5ee13cf42fb8c37f
qvissak <quinn.vissak@uconn.edu>
2018-03-13 17:06:20 -0400

    JEFF-18 Add UConn logo to Reading page

--- src/components/Reading/index.js
+++ src/components/Reading/index.js
@@ -11,6 +11,7 @@ const styles = { color: colors.siteGray }
 class Reading extends Component {
     render = () =>
       <div className="reading-container">
+        <img className="uconn-logo" alt="uconn-logo" src="assets/img/uconn-wordmark-single-black.png"></img>
         <h1 className="name-header" style={styles}>Jeff Seemann</h1>
         <NowReading />
         <PastReading />
