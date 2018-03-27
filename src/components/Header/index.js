2594bc9081971579baf66124646945bf0f91ad79
qvissak <quinn.vissak@uconn.edu>
2018-03-26 21:53:59 -0400

    JEFF-27 Mobile Responsive Header Collapse
    Make Jeff Seemann's name collapse on same line as UConn logo on mobile devices.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ const gray = { color: colors.siteGray }
 
 class Header extends Component {
     render = () =>
-      <div>
+      <div className="header-container">
         <img className="uconn-logo" alt="uconn-logo" src="assets/img/uconn-wordmark-single-black.png"></img>
         <h1 className="header-name" style={gray}>Jeff Seemann</h1>
       </div>
