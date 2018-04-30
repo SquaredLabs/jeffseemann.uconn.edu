d11f9c683ebc9811f244de2643c86d00a58c70d2
qvissak <quinn.vissak@uconn.edu>
2018-04-29 23:29:19 -0400

    JEFF-22 404 Page
    Moderately change CSS in Header for proper formatting using the document keyword.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,13 +8,16 @@ const green = { color: colors.siteGreen }
 
 class Header extends Component {
   render () {
-    const { page, hideWithNav } = this.props
+    const { page, hideWithNav, hideAlways } = this.props
+
+    const classType = hideWithNav ? 'header-name-hidden' : 'header-name'
+    const hideClass = hideAlways ? classType + ' header-hidden' : classType
 
     return <div className="header-container">
       <img className="uconn-logo" alt="uconn-logo" src="assets/img/uconn-wordmark-single-black.png"></img>
       <div className="header-wrapper">
         <h1 className="header-name" style={gray}>Jeff Seemann</h1>
-        <h1 className={hideWithNav ? 'header-name-hidden' : 'header-name'} style={green}>{page}</h1>
+        <h1 className={hideClass} style={green}>{page}</h1>
       </div>
     </div>
   }
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router'
+
+import { colors } from '../../config'
+import Header from '../Header'
+import Navigation from '../Navigation'
 
 import './styles.css'
 
+const gray = { color: colors.siteGray }
+const black = { color: colors.siteBlack }
+const lightgray = { color: colors.profileWhite }
+
 class NotFound extends Component {
-    render = () =>
-      <div>404 Page Not Found</div>
+  componentWillMount () {
+    document.body.classList.add('page-notfound')
+  }
+
+  componentWillUnmount () {
+    document.body.classList.remove('page-notfound')
+  }
+
+  render = () =>
+    <div className="notfound-container">
+      <div className="notfound-left">
+        <div className="notfound-header">
+          <Header page="Lab" hideAlways={true}/>
+        </div>
+        <div className="notfound-404" style={black}>404</div>
+        <div className="notfound-error">
+          <div style={lightgray}>https://jeffseemann.uconn.edu/</div>
+          <div style={gray}>{this.props.location.pathname.slice(1)}</div>
+          <div style={black}>Error: page not found.</div>
+        </div>
+        <div className="notfound-right-mobile">
+          <Navigation />
+        </div>
+      </div>
+      <div className="notfound-right">
+        <Navigation />
+      </div>
+    </div>
 }
 
-export default NotFound
+export default withRouter(NotFound)
