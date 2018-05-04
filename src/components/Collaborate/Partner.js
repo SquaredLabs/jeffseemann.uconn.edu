d04b0e644796bf363c510ae0fdd54f4b6492a318
qvissak <quinn.vissak@uconn.edu>
2018-05-04 14:47:19 -0400

    JEFF-45 Collaborate Drop Down Description

--- src/components/Collaborate/Partner.js
+++ src/components/Collaborate/Partner.js
@@ -7,16 +7,32 @@ const gray = { color: colors.siteGray }
 const black = { color: colors.siteBlack }
 
 class Partner extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { show: false }
+
+    this.toggleShow = this.toggleShow.bind(this)
+  }
+
+  toggleShow () { this.setState({ show: !this.state.show }) }
+
   render () {
     const { profile } = this.props
 
+    const showDescription = this.state.show
+      ? <div className="partner-description" style={black}>{profile.description}</div>
+      : <div></div>
+
     return <div className="partner-container">
-      <img className="partner-img" alt="icon" src="assets/img/Expand_Text.png"></img>
-      <div>
-        <div className="partner-title" style={black}>{profile.name}</div>
+      <div className="partner-title" style={black}>{profile.name}</div>
+      <div onClick={this.toggleShow}>
+        <img className="partner-img" alt="icon"
+          src={this.state.show ? 'assets/img/Collapse_Text.png' : 'assets/img/Expand_Text.png'}></img>
         <div className="partner-subtitle" style={gray}>
           {profile.company_name} &middot; {profile.topic_name}
         </div>
+        {showDescription}
       </div>
     </div>
   }
