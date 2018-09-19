7ed5f1b22328bb1a93ad9cf02fa2cdab4b18094e
Chris Phan <nhat.phan@uconn.edu>
2018-09-19 10:02:36 -0400

    Lift up State for Collaborators component

--- src/components/Collaborate/Collaborators.js
+++ src/components/Collaborate/Collaborators.js
@@ -1,49 +1,24 @@
-import React, { Component } from 'react'
-import { apiFetch, apiImageUrl } from '../../utils'
-import { apiUri } from '../../config'
-
+import React from 'react'
+import {  apiImageUrl } from '../../utils'
 import Partner from './Partner'
 
 import './styles.css'
 
-class Collaborators extends Component {
+const Collaborators = (props) => {
+  const collaborators = props.collaborators.map((partner, i) =>
+    <Partner key={i} profile={partner} />
+  )
+  const sponsors = props.sponsors.map((sponsor, i) => {
+    const uri = apiImageUrl(sponsor.logo.data.url)
+    return <img key={i} className="sponsor-img" alt={sponsor.name} src={uri} />
+  })
 
-
-    state = {
-      collaborators: [],
-      sponsors: []
-    
-  }
-
-  componentDidMount () {
-    this.getCollaborators()
-    this.getSponsors()
-  }
-
-  async getCollaborators () {
-    const response = await apiFetch(apiUri.collaborators.pathname, apiUri.collaborators.query)
-    this.setState({ collaborators: response.data })
-  }
-
-  async getSponsors () {
-    const response = await apiFetch(apiUri.sponsors.pathname, apiUri.sponsors.query)
-    this.setState({ sponsors: response.data })
-  }
-
-  render () {
-    const collaborators = this.state.collaborators.map((partner, i) =>
-      <Partner key={i} profile={partner} />
-    )
-    const sponsors = this.state.sponsors.map((sponsor, i) => {
-      const uri = apiImageUrl(sponsor.logo.data.url)
-      return <img key={i} className="sponsor-img" alt={sponsor.name} src={uri} />
-    })
-
-    return <React.Fragment>
+  return (
+    <React.Fragment>
       <div className="collaborator-container">{collaborators}</div>
       <div className="sponsor-container">{sponsors}</div>
     </React.Fragment>
-  }
+  )
 }
 
 export default Collaborators
--- src/components/Collaborate/index.js
+++ src/components/Collaborate/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { colors } from '../../config'
-
+import { apiFetch, apiImageUrl } from '../../utils'
+import { apiUri } from '../../config'
 import Header from '../Header'
 import Collaborators from './Collaborators'
 import Menu from '../Navigation/Menu'
-
+import ErrorHandler from '../../hoc/ErrorHandler/errorHandler'
 import './styles.css'
 
 const gray = { color: colors.siteGray }
@@ -12,9 +13,42 @@ const black = { color: colors.siteBlack }
 const green = { color: colors.siteGreen }
 
 class Collaborate extends Component {
+  state = {
+    collaborators: [],
+    sponsors:[],
+    error: false
+  }
+  componentDidMount() {
+    this.getCollaborators()
+    this.getSponsors()
+  }
+  async getCollaborators() {
+    let response = null
+    try {
+      response = await apiFetch(apiUri.collaborators.pathname, apiUri.collaborators.query)
+    } catch (error) {
+      this.setState({ error: true })
+      return;
+    }
+    this.setState({ collaborators: response.data })
+  }
+
+  async getSponsors() {
+    let response = null
+    try {
+      response = await apiFetch(apiUri.sponsors.pathname, apiUri.sponsors.query)
+    } catch (error) {
+      this.setState({ error: true })
+      return;
+    }
+
+    this.setState({ sponsors: response.data })
+  }
+
   render = () =>
+  <ErrorHandler error={this.state.error}>
     <div className="collab-container">
-      <Header page="Collaborate"/>
+      <Header page="Collaborate" />
       <div className="collab-welcome-banner">
         <div>
           <div className="collab-name" style={black}>Seemann</div>
@@ -28,12 +62,13 @@ class Collaborate extends Component {
         <div className="collab-slogan" style={green}>Let's share.</div>
       </div>
       <div className="collab-collab" style={gray}>Past + Present Collaborators</div>
-      <Collaborators />
+      <Collaborators collaborators ={this.state.collaborators} sponsors={this.state.sponsors}/>
       <div className="collab-slogan-container">
-        <div className="collab-slogan" style={gray}>Interested? Contact <span style={black}><u>Jeff</u></span>.</div>
+        <div className="collab-slogan" style={gray}>Interested? Contact <a style={black} href="mailto:@jeff.seeman@uconn.edu">Jeff</a>.</div>
       </div>
       <Menu />
     </div>
+    </ErrorHandler>
 }
 
 export default Collaborate
